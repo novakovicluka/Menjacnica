9681a949c1bb2ab180d2a88ae295e7c36a0a65b2
Luka <Luka@192.168.1.103>
2016-03-24 23:52:56 +0100

    Isplementirane metode Klase implementiraIntefejs

--- Menjacnica/src/implementiraInterfejs.java
+++ Menjacnica/src/implementiraInterfejs.java
@@ -1,22 +1,43 @@
+import java.util.LinkedList;
+
 import paketzaspec.Interfejs;
 
 public class implementiraInterfejs implements Interfejs {
 
-	@Override
-	public void dodajKurs() {
+	LinkedList<Valuta> valute = new LinkedList<>();
+	public void dodajKurs(String valuta,double kurs) {
+	for (int i = 0; i < valuta.length();i++) {
+		if(valute.get(i).getIme().equals(valuta))
+			{
+			valute.get(i).setKursKupovni(kurs+0.5);
+			valute.get(i).setKursSrednji(kurs);
+			valute.get(i).setKursProdajni(kurs-0.5);
+			}}	
 		
 	}
 
 	@Override
-	public void obrisiKurs() {
-		
+	public void obrisiKurs(String valuta) {
+		for (int i = 0; i < valuta.length(),i++) {
+			if(valute.get(i).getIme().equals(valuta)) {
+				valute.get(i).setKursKupovni(0);
+				valute.get(i).setKursSrednji(0);
+				valute.get(i).setKursProdajni(0);
+			}
+		}	
 
 	}
 
 	@Override
-	public double vratiKurs() {
+	public double vratiKurs(String valuta) {
+		double vratiKurs;
+		for (int i = 0; i < valuta.length(),i++) {
+			if(valute.get(i).getIme().equals(valuta)) {
+				return valute.get(i).getKursSrednji();
+				
+			}
+		}	
 		
-		return 0;
 	}
 
 }
