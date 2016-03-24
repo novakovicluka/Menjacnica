686ad19d2a33dcaaee2ae6a7c878093373f1f28b
Luka <Luka@192.168.1.103>
2016-03-24 22:39:00 +0100

    Dodata klasa Valuta sa svojim atributima i geterima i seterima

---
+++ Menjacnica/src/Valuta.java
@@ -0,0 +1,42 @@
+
+public class Valuta {
+String ime;
+String skr;
+double kursProdajni;
+double kursSrednji;
+double kursKupovni;
+public String getIme() {
+	return ime;
+}
+public void setIme(String ime) {
+	this.ime = ime;
+}
+public String getSkr() {
+	return skr;
+}
+public void setSkr(String skr) {
+	this.skr = skr;
+}
+public double getKursProdajni() {
+	return kursProdajni;
+}
+public void setKursProdajni(double kursProdajni) {
+	this.kursProdajni = kursProdajni;
+}
+public double getKursSrednji() {
+	return kursSrednji;
+}
+public void setKursSrednji(double kursSrednji) {
+	this.kursSrednji = kursSrednji;
+}
+public double getKursKupovni() {
+	return kursKupovni;
+}
+public void setKursKupovni(double kursKupovni) {
+	this.kursKupovni = kursKupovni;
+}
+
+
+
+
+}
