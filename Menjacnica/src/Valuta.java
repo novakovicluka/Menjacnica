478cf94c0a70e357663b974f888358f9d6d83d69
Luka <Luka@192.168.1.103>
2016-03-24 22:41:01 +0100

    Dodati HashCode,Equals,toString u klasu Valuta

--- Menjacnica/src/Valuta.java
+++ Menjacnica/src/Valuta.java
@@ -35,6 +35,53 @@ public double getKursKupovni() {
 public void setKursKupovni(double kursKupovni) {
 	this.kursKupovni = kursKupovni;
 }
+@Override
+public String toString() {
+	return "ime: " + ime + ", skr: " + skr + ", kursProdajni: " + kursProdajni + ", kursSrednji: " + kursSrednji
+			+ ", kursKupovni: " + kursKupovni;
+}
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((ime == null) ? 0 : ime.hashCode());
+	long temp;
+	temp = Double.doubleToLongBits(kursKupovni);
+	result = prime * result + (int) (temp ^ (temp >>> 32));
+	temp = Double.doubleToLongBits(kursProdajni);
+	result = prime * result + (int) (temp ^ (temp >>> 32));
+	temp = Double.doubleToLongBits(kursSrednji);
+	result = prime * result + (int) (temp ^ (temp >>> 32));
+	result = prime * result + ((skr == null) ? 0 : skr.hashCode());
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Valuta other = (Valuta) obj;
+	if (ime == null) {
+		if (other.ime != null)
+			return false;
+	} else if (!ime.equals(other.ime))
+		return false;
+	if (Double.doubleToLongBits(kursKupovni) != Double.doubleToLongBits(other.kursKupovni))
+		return false;
+	if (Double.doubleToLongBits(kursProdajni) != Double.doubleToLongBits(other.kursProdajni))
+		return false;
+	if (Double.doubleToLongBits(kursSrednji) != Double.doubleToLongBits(other.kursSrednji))
+		return false;
+	if (skr == null) {
+		if (other.skr != null)
+			return false;
+	} else if (!skr.equals(other.skr))
+		return false;
+	return true;
+}
 
 
 
