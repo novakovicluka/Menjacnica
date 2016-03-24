6a1d11076ff50ede7321b354687fe1a419fb8f91
Luka <Luka@192.168.1.103>
2016-03-25 00:04:09 +0100

    Druga verzija implementacije na master grani

--- Menjacnica/src/implementiraInterfejs.java
+++ Menjacnica/src/implementiraInterfejs.java
@@ -1,21 +1,48 @@
+import java.util.LinkedList;
+
 import paketzaspec.Interfejs;
 
 public class implementiraInterfejs implements Interfejs {
-
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 	@Override
-	public void dodajKurs() {
-		
+	public void dodajKurs(String valuta,double prodajni,double kupovni,double srednji) {
+		Valuta val = new Valuta();
+		val.setIme(valuta);
+		val.setKursKupovni(kupovni);
+		val.setKursProdajni(prodajni);
+		val.setKursSrednji(srednji);
+		for (int i = 0; i < valuta.length(),i++) {
+			if(valute.get(i).getIme().equals(valuta)) {
+				valute.get(i) = val;
+			}
+		}	
 	}
 
 	@Override
-	public void obrisiKurs() {
-		
+	public void obrisiKurs(String valuta){
+		Valuta val = new Valuta();
+		val.setIme(valuta);
+		val.setKursKupovni(0);
+		val.setKursProdajni(0);
+		val.setKursSrednji(0);
+		for (int i = 0; i < valuta.length(),i++) {
+			if(valute.get(i).getIme().equals(valuta)) {
+				valute.get(i) = val;
+			}
+		}	
 
 	}
 
 	@Override
-	public double vratiKurs() {
+	public double vratiKurs(String valuta) {
 		
+		
+		
+		for (int i = 0; i < valuta.length(),i++) {
+			if(valute.get(i).getIme().equals(valuta)) {
+				return valute.get(i).getKursSrednji();
+			}
+		}	
 		return 0;
 	}
 
